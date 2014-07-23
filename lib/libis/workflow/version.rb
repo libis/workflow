6e88e2e787c0fc5e937b177e3a332fa802ef0dad
Kris Dekeyser <kris.dekeyser@libis.be>
2014-07-23 14:41:23 +0200

    release version 1.0

--- lib/libis/workflow/version.rb
+++ lib/libis/workflow/version.rb
@@ -2,6 +2,6 @@
 
 module LIBIS
   module Workflow
-    VERSION = '0.1.1' unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
+    VERSION = '1.0' unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
   end
 end
