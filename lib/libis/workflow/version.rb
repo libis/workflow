f1ec381230daf6792f7d210e37a4b63ef15ff42b
Kris Dekeyser <kris.dekeyser@kuleuven.be>
2023-09-06 12:38:19 +0200

    version bump: 2.1.12

--- lib/libis/workflow/version.rb
+++ lib/libis/workflow/version.rb
@@ -1,5 +1,5 @@
 module Libis
   module Workflow
-    VERSION = '2.1.11' unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
+    VERSION = '2.1.12' unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
   end
 end
